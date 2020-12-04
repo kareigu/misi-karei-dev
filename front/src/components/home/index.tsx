0e45edda5c61b22a1129eceef9aa1419b0c4edf8
mxr <eetu.kivelae@gmail.com>
2020-12-04 21:46:09 +0200

    Added permission getter to home page

--- front/src/components/home/index.tsx
+++ front/src/components/home/index.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import './Home.css';
 import Logo from '../../temp.gif'
 
+function perms() {
+  localStorage.setItem('token', 'asdasda');
+}
+
 function Home() {
   return (
-    <img id="logo" src={Logo} alt="kettukaani"/>
+    <div>
+      <img id="logo" src={Logo} alt="kettukaani"/>
+      <button onClick={perms}>Get perms</button>
+    </div>
   );
 }
 
