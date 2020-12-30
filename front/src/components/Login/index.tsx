37ae2f05288e6a269007dd32d13d1ce45acf2ae7
mxr <eetu.kivelae@gmail.com>
2020-12-30 14:26:23 +0200

    Added signing out

--- front/src/components/Login/index.tsx
+++ front/src/components/Login/index.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import checkLogin from '../../utils/Login';
 
 type props = {
-  path: string,
+  path: 'login' | 'signout',
   loginState: {}
 }
 
@@ -12,22 +12,50 @@ function Login(props: props) {
     console.log(password);
     checkLogin(password)
               .then(success => {
-                if(success)
-                  window.location.href = '/'
+                if(success) {
+                  setStatus('Logged in');
+                  window.location.href = '/';
+                } else
+                  setStatus('Wrong password');
               })
   }
 
+  function handleSignout() {
+    localStorage.removeItem('token');
+    setTimeout(() => setStatus('Signed out'), 200);
+    setTimeout(() => window.location.href = '/', 500);
+  }
+
+  useEffect(() => {
+    if(props.path === 'signout')
+      handleSignout();
+  })
+
   const [password, setPassword] = useState('');
+  const [status, setStatus] = useState(props.path === 'login' ? 'Login' : 'Signing out');
   return (
     <div>
-      <h1>Login</h1>
-      <input 
-        type="text" 
-        value={password}
-        onChange={e => setPassword(e.currentTarget.value)}
-      >
-      </input>
-      <button onClick={handleSubmit}>Login</button>
+      {
+        props.path === 'login' &&
+        <div className="login">
+          <h1>{status}</h1>
+          <input 
+            type="text" 
+            value={password}
+            onChange={e => setPassword(e.currentTarget.value)}
+          >
+          </input>
+          <button onClick={handleSubmit}>Login</button>
+        </div>
+      }
+
+      {
+        props.path === 'signout' &&
+        <div>
+          <h1>{ status }</h1>
+        </div>
+      }
+      
     </div>
   );
 }
