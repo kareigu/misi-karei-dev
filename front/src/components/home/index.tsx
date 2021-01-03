746b41037f65583ece4caacf213e71f4b2876070
mxr <eetu.kivelae@gmail.com>
2021-01-04 00:02:19 +0200

    Removed useless legacy button

--- front/src/components/home/index.tsx
+++ front/src/components/home/index.tsx
@@ -2,15 +2,11 @@ import React from 'react';
 import './Home.css';
 import Logo from '../../temp.gif'
 
-function perms() {
-  console.log('deprecated');
-}
 
 function Home() {
   return (
     <div>
       <img id="logo" src={Logo} alt="kettukaani"/>
-      <button onClick={perms}>Get perms</button>
     </div>
   );
 }
