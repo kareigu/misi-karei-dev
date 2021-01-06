495edc606ff0bb48c105a2b4d4331587953630db
mxr <eetu.kivelae@gmail.com>
2021-01-06 02:45:45 +0200

    Fixed crash

--- api/v1/utils/users/updateUserPerms.js
+++ api/v1/utils/users/updateUserPerms.js
@@ -3,19 +3,22 @@ module.exports = async function(db, data) {
   
   const updatedValues = [];
 
-  data.forEach(el => {
-    if(typeof el.permissionLevel === 'number') {
-      db.findOneAndUpdate(
-          {id: el.id},
-          {$set: {
-            permissionLevel: el.permissionLevel
-          }}
-      )
-      updatedValues.push(`Updated ${el.id} permissions to level: ${el.permissionLevel}`);
-    } else {
-      updatedValues.push(`ID ${el.id} has invalid permissionLevel ${el.permissionLevel}`);
-    }
-  });
-
+  try {
+    data.forEach(el => {
+      if(typeof el.permissionLevel === 'number') {
+        db.findOneAndUpdate(
+            {id: el.id},
+            {$set: {
+              permissionLevel: el.permissionLevel
+            }}
+        )
+        updatedValues.push(`Updated ${el.id} permissions to level: ${el.permissionLevel}`);
+      } else {
+        updatedValues.push(`ID ${el.id} has invalid permissionLevel ${el.permissionLevel}`);
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
   return updatedValues;
 }
