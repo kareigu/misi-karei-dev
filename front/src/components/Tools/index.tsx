8229333ebb9d741717a00c1f4e82ce31a61ad8ea
mxr <eetu.kivelae@gmail.com>
2021-01-07 23:16:59 +0200

    Added loading backdrop

--- front/src/components/Tools/index.tsx
+++ front/src/components/Tools/index.tsx
@@ -1,19 +1,41 @@
 import React from 'react';
 
-import { Container } from '@material-ui/core'
+import { Backdrop, Container, createStyles, makeStyles, Theme } from '@material-ui/core'
 
 import StreamNotify from '../StreamNotify';
 import UserList from '../UserList';
 import './Tools.css';
+import { useLayoutEffect, useState } from 'react';
+
+import { ReactComponent as LoadingIcon } from '../../utils/loading2.svg'
 
 interface Props {
   permLevel: number
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+      color: '#fff',
+    },
+  }),
+);
+
 function Tools(props: Props) {
+  const classes = useStyles();
+
+  const [loading, setLoading] = useState(true);
+
+  useLayoutEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  });
 
   return (
     <div>
+      <Backdrop className={classes.backdrop} open={loading}>
+        <LoadingIcon />
+      </Backdrop>
       <header className="Misc-header">
         <h2>Tools</h2>
       </header>
