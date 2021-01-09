08f1fc88c4a73823cf1cac4c0d7f53e55bb8ae1d
mxr <eetu.kivelae@gmail.com>
2021-01-10 01:26:54 +0200

    Added UI for downloading and adding quotes

--- front/src/components/QuoteList/index.tsx
+++ front/src/components/QuoteList/index.tsx
@@ -1,21 +1,33 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import './QuoteList.css'
 import GetFullQuoteList from '../../utils/getData';
 import QuoteBlock from '../QuoteBlock';
 import PageSwitcher from '../PageSwitcher';
+import paths from '../../utils/paths.json';
+
 
 import { ReactComponent as LoadingIcon } from '../../utils/loading2.svg';
+import { Button, TextField } from '@material-ui/core';
+import { CloudDownload, Add, Close } from '@material-ui/icons';
+import UserContext from '../../utils/UserContext';
 
 interface props {
   source: 'quotes' | 'niilo'
 }
 
+const reqPath = process.env.NODE_ENV === 'development' ? paths.devPath : paths.productionPath;
+
+
 function QuoteList(props: props) {
+
   const service = GetFullQuoteList(props.source);
   const [list, setList] = useState<Array<JSX.Element>>();
   //const [searchTerm, setSearchTerm] = useState('');
   const [pageCount, setPageCount] = useState(1);
   const [finalPage, setFinalPage] = useState(1);
+  const [addingContent, setAddingContent] = useState(false);
+  const [contentToAdd, setContentToAdd] = useState('');
+  const {user} = useContext(UserContext);
 
   useEffect(() => {
     let tempArr: Array<JSX.Element> = [];
@@ -45,7 +57,74 @@ function QuoteList(props: props) {
   return (
     <div>
       { service.status === 'loaded' &&
+      <>
+        <div 
+          style={{
+            display: 'flex',
+            backgroundColor: 'rgba(255, 255, 255, 0.2)',
+            padding: '15px 0px'
+          }}
+        >
+        { user.permLevel >= 3 &&
+        <>
+        { !addingContent &&
+          <Button
+            style={{left: '25px'}}
+            variant="contained"
+            color="secondary"
+            onClick={() => setAddingContent(true)}
+          >
+            <Add /> Add {props.source === 'quotes' ? 'Quote' : 'Niilo'}
+          </Button>
+        }
+
+        { addingContent &&
+        <>
+          <TextField
+            id="custom-css-standard-input"
+            label={props.source === 'quotes' ? 'quote' : props.source}
+            style={{left: '25px', width: '400px', height: '25px', bottom: '10px'}}
+            inputProps={{ 'aria-label': 'naked' }}
+            value={contentToAdd}
+            onChange={e => setContentToAdd(e.currentTarget.value)}
+          >
+          </TextField>
+
+          <Button
+            style={{marginLeft: '28px'}}
+            size="small"
+            variant="contained"
+            color="primary"
+            onClick={() => setAddingContent(false)}
+          >
+            <Add /> Add
+          </Button>
+
+          <Button
+            style={{marginLeft: '5px'}}
+            size="small"
+            variant="contained"
+            color="default"
+            onClick={() => setAddingContent(false)}
+          >
+            <Close /> Cancel
+          </Button>
+        </>
+        }
+          
+        </>
+        }
+        <Button
+          style={{marginLeft: 'auto', marginRight: '15px'}}
+          variant="contained"
+          color="primary"
+          onClick={() => window.location.href = `${reqPath}${props.source}/backup`}
+        >
+          <CloudDownload /> &nbsp; Download {props.source}
+        </Button>
+        </div>
         <PageSwitcher pageCount={pageCount} finalPage={finalPage} setPageCount={setPageCount} />
+      </>
       }
       
       <div className="quotelist-container">
