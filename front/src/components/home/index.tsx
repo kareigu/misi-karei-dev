cb7b63406d6a6c2ff884a2a85bd3f815497d09cc
mxr <eetu.kivelae@gmail.com>
2021-01-13 17:49:14 +0200

    Added daily niilo and quote to home page

--- front/src/components/home/index.tsx
+++ front/src/components/home/index.tsx
@@ -1,12 +1,71 @@
 import React from 'react';
 import './Home.css';
-import Logo from '../../temp.gif'
+import { Badge, Grid, Card, CardContent, Typography } from '@material-ui/core';
+import { useState, useEffect } from 'react';
+import LoadingComponent from '../LoadingComponent';
+
+import paths from '../../utils/paths.json';
+const reqPath = process.env.NODE_ENV === 'development' ? paths.devPath : paths.productionPath;
+
+interface IHomeContent {
+  [key: string]: {
+    _id: string,
+    text: string,
+    number: number
+  }
+}
 
 
 function Home() {
+  const [homeContent, setHomeContent] = useState<IHomeContent>();
+
+  useEffect(() => {
+    fetch(`${reqPath}home/content`)
+      .then(res => res.json())
+      .then(json => setHomeContent(json));
+  }, []);
+
   return (
-    <div>
-      <img id="logo" src={Logo} alt="kettukaani"/>
+    <div id="homeComponent">
+      { !homeContent &&
+        <LoadingComponent margin="150px" />
+      }
+      
+      { homeContent &&
+        <Grid container spacing={2} className="homeContainer">
+          <Grid item xs={window.innerWidth < 640 ? 12 : 6}>
+              <Card className="contentCard">
+                <CardContent className="cardContent">
+                  <Typography className="cardTitle">
+                    <Badge badgeContent={homeContent.dailyQuotes.number} max={9999} color="primary">
+                      Päivän quote &nbsp; &nbsp; &nbsp;
+                    </Badge>
+                  </Typography>
+                  <Typography>
+                    {homeContent.dailyQuotes.text}
+                  </Typography>
+                </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item xs={window.innerWidth < 640 ? 12 : 6}>
+            <Card className="contentCard">
+              <CardContent className="cardContent">
+                <Typography className="cardTitle">
+                  <Badge badgeContent={homeContent.dailyNiilo.number} max={9999} color="secondary">
+                    Päivän niilo &nbsp; &nbsp; &nbsp;
+                  </Badge>
+                </Typography>
+                <Typography>
+                  {homeContent.dailyNiilo.text}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+
+        </Grid>
+      }
+      
     </div>
   );
 }
