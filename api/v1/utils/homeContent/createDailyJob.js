7c19b37ca328ef340756912ab4e5b82668d23a94
mxr <eetu.kivelae@gmail.com>
2021-01-13 21:11:59 +0200

    Changed to production values

--- api/v1/utils/homeContent/createDailyJob.js
+++ api/v1/utils/homeContent/createDailyJob.js
@@ -7,7 +7,7 @@ module.exports = function(db, collection) {
 
   const typeName = 'daily' + collection.charAt(0).toUpperCase() + collection.slice(1);
 
-  const job = new CronJob('15,30,45 * * * * *', () => {
+  const job = new CronJob('0 0 * * *', () => {
     randomQuote(source).then(quote => {
       try {
         homeContent.findOneAndUpdate({type: typeName}, {$set: {value: quote.number}});
